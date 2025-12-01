44bee8a6a23a3803fecf4841b84ad0576a337c96
Ishwarya Gandamsetty <igandam+ncstate@ncsu.edu>
2025-12-01 13:10:05 -0500

    created 2 new files for calculate distance, API endpoints for nearby pools and register new routes in the backend

--- Proj2/backend/routes/__init__.py
+++ Proj2/backend/routes/__init__.py
@@ -3,8 +3,7 @@ from flask import Blueprint
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-from . import health, groups, polls, auth_routes, profile, orders
-
+from . import health, groups, polls, auth_routes, profile, orders, discovery
 # Register the auth blueprint with the main API blueprint
 bp.register_blueprint(auth_routes.auth_bp, url_prefix='/auth')
 bp.register_blueprint(profile.profile_bp, url_prefix='/profile')
